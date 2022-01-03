09928aae5401bcc2e3b953827887ea64a76d9db9
TheAndrew-O <76549716+TheAndrew-O@users.noreply.github.com>
2022-01-02 15:21:42 -1000

    Update NewEgg.py

--- NewEgg.py
+++ NewEgg.py
@@ -18,7 +18,10 @@ accountSid = 'ID'
 authToken = 'TOKEN'
 client = Client(accountSid, authToken)
 
-# Product Page 
+#Make Sure that credit card information is already added to Newegg/account.
+
+# Product Page
+# URL Must be Newegg website
 url = 'https://www.newegg.com/p/pl?d=rtx+3070&N=100007709&isdeptsrh=1'
 
 def timeSleep(x, driver):
@@ -38,7 +41,7 @@ def createDriver():
     options = Options()
     # Change To False if you want to see Firefox Browser Again.
     options.headless = True
-    # Enter Firefox Profile Here in quotes.
+    # Change Profile Here in quotes.
     profile = webdriver.FirefoxProfile(
         r'C:\Users\USER\AppData\Roaming\Mozilla\Firefox\Profiles\CODE.default-release')
     driver = webdriver.Firefox(profile, options=options, executable_path=GeckoDriverManager().install())
@@ -52,6 +55,7 @@ def buyCard(driver):
             wait = WebDriverWait(driver, 15)
             add_Cart = driver.find_element_by_xpath("//*[@class='btn btn-primary btn-mini']")
             add_Cart.click()
+            # Uncomment following line to use on Best Buy websites
             #add_Cart = driver.find_element_by_xpath('//*[@id="ProductBuy"]/div/div[2]/button')
             #add_Cart.click()
             print("Item found, adding to cart")
@@ -63,6 +67,7 @@ def buyCard(driver):
                 driver.find_element_by_xpath("//*[@class='btn btn-primary btn-wide']").click()
                 print("Item Is Still In Cart.")
                 try:
+                    # Send Message to phone that item is ready for purchase
                     client.messages.create(to=toNumber, from_=fromNumber, body='IN CART')
                 except (NameError, TwilioRestException):
                     pass
@@ -72,6 +77,7 @@ def buyCard(driver):
                 buyCard(driver)
                 return
             try:
+                # If added item to cart and still in cart, then proceed with transaction
                 wait.until(
                     EC.presence_of_element_located((By.XPATH, "//*[@id='signInSubmit']")))
                 driver.find_element_by_xpath("//*[@id='signInSubmit']").click()
@@ -80,6 +86,7 @@ def buyCard(driver):
                 security_code = driver.find_element_by_xpath(
                     '//*[@id="app"]/div/section/div/div/form/div[2]/div[1]/div/div[3]/div/div[2]/div[1]/div[2]/div[3]/input')
                 time.sleep(1)
+                # Change CVV number
                 security_code.send_keys('123')
                 driver.find_element_by_xpath('//*[@id="btnCreditCard"]').click()
                 print('Order Placed!')
@@ -90,16 +97,18 @@ def buyCard(driver):
             except (NoSuchElementException, TimeoutException):
                 print("Already signed in")
                 try:
+                    # If order not placed because website needs to sign in again
                     security_code = driver.find_element_by_xpath('//*[@id="app"]/div/section/div/div/form/div[2]/div[1]/div/div[3]/div/div[2]/div[1]/div[2]/div[3]/input')
                     print("Entering key")
                     time.sleep(1)
+                    # Change CVV
                     security_code.send_keys('123')
                     print("entered")
                     driver.find_element_by_xpath('//*[@id="btnCreditCard"]').click()
                     print('Order Placed!')
                     driver.quit()
                 except NoSuchElementException:
-                    print("dude")
+                    print("Failed to sign in again.")
                     driver.quit()
                     return
                 try:
@@ -109,7 +118,6 @@ def buyCard(driver):
                     return
 
         except NoSuchElementException:
-            # print('nope')
             pass
         timeSleep(5, driver)
 
